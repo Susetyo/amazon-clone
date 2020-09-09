77cd8dd98e28d0fa5d529d5084fe5376de1c9145
Susetya Mulia Pambudi <susetyamulia@gmail.com>
2020-09-10 02:40:07 +0700

    Homework DAY-2

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "./StateProvider";
 
 function Subtotal() {
+    const [{ basket }, dispatch] = useStateValue();
+
+    const getBasketTotal = (basket) => {
+        let total = 0;
+        basket.map((b) => (total += b.price));
+        return total;
+    };
+
     return (
         <div className="subtotal">
             <CurrencyFormat
                 renderText={(value) => (
                     <>
                         <p>
-                            Subtotal (0 items):
-                            <strong>0</strong>
+                            Subtotal ({basket.length} items):
+                            <strong>{`${value}`}</strong>
                         </p>
                         <small className="subtotal__gift">
                             <input type="checkbox" /> This order contains a gift
@@ -18,8 +27,7 @@ function Subtotal() {
                     </>
                 )}
                 decimalScale={2}
-                // value={getBasketTotal(basket)}
-                value={0}
+                value={getBasketTotal(basket)}
                 displayType={"text"}
                 thousandSeparator={true}
                 prefix={"$"}
